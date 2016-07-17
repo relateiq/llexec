72480ceb519019c739ee22d31247cf4e204cbd08
Joe C <joechrz@gmail.com>
2016-07-17 11:41:28 -0700

    fix(wrappers/cmdname): make it actually only print the command name

--- src/canned-wrappers/cmdname-color/index.ts
+++ src/canned-wrappers/cmdname-color/index.ts
@@ -3,13 +3,17 @@ import * as util from '../../util';
 
 const colors = require('colors');
 
+function getCommandName(command: string) {
+  return command.split(/\s/g).shift() || '';
+}
+
 let maxCmdLength = null;
 export function lineWrapper(taskInfo: ITaskInfo, line: string): string {
   if (maxCmdLength === null) {
-    maxCmdLength = util.getMaxCmdLength(taskInfo.allTasks);
+    maxCmdLength = util.getMaxLength(taskInfo.allTasks.map(t => getCommandName(t.command)));
   }
 
-  const taskName = taskInfo.task.command;
+  const taskName = getCommandName(taskInfo.task.command);
   const colorFunc = util.getColorFuncForIndex(taskInfo.taskIndex);
 
   return colorFunc(util.rightPad(taskName, maxCmdLength) + ' |') + ' ' + line;
